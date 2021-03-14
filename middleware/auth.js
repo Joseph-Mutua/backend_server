c1ade5a51a9a0dc88784acfe714375d377cb7970
Joseph Mutua <mutuaj793@gmail.com>
2021-03-14 17:01:39 +0300

    create or update user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,19 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers);
-  next();
+exports.authCheck = async (req, res, next) => {
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+
+    console.log("FIREBASE USER IN AUTH CHECK", firebaseUser);
+    req.user = firebaseUser;
+    next();
+  } catch (err) {
+    console.log(err.message);
+    res.status(401).json({
+      // err: err.message
+      err: "Invalid or Expired token",
+    });
+  }
 };
