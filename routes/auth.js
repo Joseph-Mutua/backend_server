acc997fe28f2a52509709c07bfa6f387ad11c449
Joseph Mutua <mutuaj793@gmail.com>
2021-03-15 16:13:03 +0300

    Current user endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createOrUpdateUser } = require("../controllers/auth");
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
 //Middleware
 const { authCheck } = require("../middleware/auth");
 
 //Routes
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+
+router.post("/current-user", authCheck, currentUser);
 
 module.exports = router;
