352782536fa2b447114772a661366de36b999485
Joseph Mutua <mutuaj793@gmail.com>
2021-03-18 22:23:22 +0300

    Add subcategories controller

---
+++ controllers/subcategory.js
@@ -0,0 +1,54 @@
+const SubCategory = require("../models/subcategory");
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await new SubCategory({
+      name,
+      slug: slugify(name).toLowerCase(),
+    }).save();
+    res.json(subcategory);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).send("Create subcategory failed");
+  }
+};
+
+exports.list = async (req, res) => {
+  res.json(await SubCategory.find({}).sort({ createdAt: -1 }).exec());
+  //
+};
+
+exports.read = async (req, res) => {
+  let subcategory = await SubCategory.findOne({ slug: req.params.slug }).exec();
+  res.json(category);
+  //
+};
+
+exports.update = async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const updated = await SubCategory.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("SubCategory update failed!");
+  }
+  //
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await SubCategory.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("SubCategory delete failed!");
+  }
+};
