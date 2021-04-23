d42713015734d6c8b5efddda12e221fc3d23b31d
Joseph Mutua <mutuaj793@gmail.com>
2021-04-23 14:15:23 +0300

    Empty cart function

--- controllers/user.js
+++ controllers/user.js
@@ -57,12 +57,19 @@ exports.userCart = async (req, res) => {
 exports.getUserCart = async (req, res) => {
   const user = await User.findOne({ email: req.user.email }).exec();
 
-  let cart = await Cart.findOne({ orderedBy: user._id }).populate(
-    "products.product",
-    "_id title price totalAfterDiscount"
-  ).exec();
+  let cart = await Cart.findOne({ orderedBy: user._id })
+    .populate("products.product", "_id title price totalAfterDiscount")
+    .exec();
 
-  const {products, cartTotal, totalAfterDiscount} = cart;
+  const { products, cartTotal, totalAfterDiscount } = cart;
 
   res.json({ products, cartTotal, totalAfterDiscount });
+};
+
+exports.emptyCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  const cart = await Cart.findOneAndRemove({ orderedBy: user._id }).exec();
+
+  res.json(cart)
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { authCheck } = require("../middleware/auth");
 
-const { userCart, getUserCart } = require("../controllers/user");
+const { userCart, getUserCart, emptyCart } = require("../controllers/user");
 
 const router = express.Router();
 
 router.post("/user/cart", authCheck, userCart);
-router.get("/user/cart", authCheck, getUserCart)
+router.get("/user/cart", authCheck, getUserCart);
+router.delete("/user/cart", authCheck, emptyCart);
 
 module.exports = router;
