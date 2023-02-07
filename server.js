8deeb8d711262235c771fd96e090465418ac6a3e
Joseph Mutua <Mutuaj793@gmail.com>
2023-02-07 12:10:35 +0300

    Remove strictquery option

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    strictQuery: true
   })
   .then(() => console.log("DB Connected"))
   .catch((err) => {
