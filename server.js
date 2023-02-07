2c0e500c99bae2afb0adc6072b7f7063dba06670
Joseph Mutua <Mutuaj793@gmail.com>
2023-02-07 12:06:45 +0300

    Mongoose fix

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ const app = express();
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
     useUnifiedTopology: true,
+    strictQuery: true
   })
   .then(() => console.log("DB Connected"))
   .catch((err) => {
